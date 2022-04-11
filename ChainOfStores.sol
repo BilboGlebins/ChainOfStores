1419161cc5373b3b18cb1c8237488bfa2dc241e2
Graule Gleb <62739961+BilboGlebins@users.noreply.github.com>
2022-04-11 13:44:30 +0700

    Add files via upload

---
+++ ChainOfStores.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+contract ChainOfStores{
+
+    struct User{
+        uint Role;
+        string LastName;
+        string FirstName;
+        string MiddleName;
+        bytes32 Password;
+        uint Balance;
+        bool isExist;
+    }
+
+    struct Product{
+        uint IdProduct;
+        uint Price;
+    }
+
+    struct Shop{
+        Product[] products;
+    }
+
+    Shop[] shops;
+
+    mapping (address => User) private users;
+
+    mapping (address => Shop) private shop;
+
+    address[] reg;
+
+    function encoderTest(string memory _text) private pure returns(bytes32){
+        return keccak256(abi.encodePacked(_text));
+    }
+
+    function pushNewUser (uint _Role, string memory _LastName, string memory _FirstName, string memory _MiddleName, string memory _Password, uint _Balance) public{
+        require(users[msg.sender].isExist == false, "Users already exist");
+        users[msg.sender] = User(
+            _Role,
+            _LastName,
+            _FirstName,
+            _MiddleName,
+            encoderTest(_Password),
+            _Balance,
+            true 
+        );
+        reg.push(msg.sender);
+    }
+
+    function autUser(string memory _Password) public view returns (bool){
+        require(users[msg.sender].isExist == true, "User not exist");
+        if (users[msg.sender].Password == encoderTest(_Password))
+        return true;
+
+        return false;
+    }
+
+    function upBeforeCustomer(address adr) public{
+        require(users[msg.sender].Role == 1, "You are admin");
+        require(users[adr].Role == 3, "You not customer");
+        users[adr].Role = 2;
+    } 
+
+    function lowerBeforeSeller(address adr) public{
+        require(users[msg.sender].Role == 1, "You are admin");
+        require(users[adr].Role == 2, "You not seller");
+        users[adr].Role = 3;
+    } 
+
+    address[] addresse;
+    uint[] roles;
+
+    mapping (address => uint) private requestrole;
+
+    function requestLowerBeforeSeller() public{
+        require(users[msg.sender].Role != 1, "You are not admin");
+        if (users[msg.sender].Role == 2){
+            requestrole[msg.sender] = 2;
+            addresse.push(msg.sender);
+            roles.push(2);
+        }
+        else if (users[msg.sender].Role == 3){
+            requestrole[msg.sender] = 3;
+            addresse.push(msg.sender);
+            roles.push(3);
+        }
+    }
+
+    string[] nameproduct;
+
+    function addShop(uint indexShop) public{
+        indexShop;
+        shops.push();
+    }
+
+    function addProduct(uint indexShop) public {
+        if (indexShop == 0){
+        shops[indexShop].products.push(Product(1, 10));
+        shops[indexShop].products.push(Product(2, 11));
+        shops[indexShop].products.push(Product(3, 176));
+        }
+        if (indexShop == 1){
+        shops[indexShop].products.push(Product(4, 90));
+        shops[indexShop].products.push(Product(5, 152));
+        shops[indexShop].products.push(Product(6, 175));
+        }
+
+        if (indexShop == 2){
+        shops[indexShop].products.push(Product(7, 74));
+        shops[indexShop].products.push(Product(8, 150));
+        shops[indexShop].products.push(Product(9, 462));
+        }
+        if (indexShop == 3){
+        shops[indexShop].products.push(Product(1, 40));
+        shops[indexShop].products.push(Product(2, 450));
+        shops[indexShop].products.push(Product(3, 4413));
+        }
+    }
+
+    //function buyProduct(uint indexShop, uint _idproduct, uint _Price, uint count) public{
+    //    require(users[msg.sender].Role != 3, "You not customer");
+    //    if (indexShop == 0){
+    //        require(msg.value >= Product[_idproduct].Price);
+    //    }
+    //    if (indexShop == 1){
+    //        
+    //    }
+    //
+    //    if (indexShop == 2){
+    //        
+    //    }
+    //    if (indexShop == 3){
+    //        
+    //    }
+    //} 
+}   
